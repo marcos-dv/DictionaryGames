af0cb66bc3ba4e9d3929ae0e04c0aa147105efe6
marcos-dv <61439029+marcos-dv@users.noreply.github.com>
2020-05-04 17:59:41 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,3 @@ int main(){
 	return 0;
 
 }
-
-// Pruebas rapidas (ej 300)
-// rm DicPalabras.o main.o TablaHash.o Arbol.o Interprete.o; make; clear; echo -n "Tiempo Transcurrido:"; time ./a.out < 200a.in > salida300
